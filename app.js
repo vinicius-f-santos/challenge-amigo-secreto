51f063839eb4b072570d5c1cce296f850f0b0f8e
Vinicius Freitas <vini.programer@gmail.com>
2025-02-13 15:02:57 -0300

    adicionar funções do projeto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let campoAmigos = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let amigoSorteado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+    if (campoAmigos.value == ''){
+        alert('Por favor, insira um nome');
+    }
+    else {
+        if (amigos.includes(campoAmigos.value)) {
+            alert('Esse amigo já está na lista');
+            limparCampo();
+        }
+        else {
+            amigos.push(campoAmigos.value);
+            mostrarAmigoNaLista(campoAmigos.value);
+            limparCampo();
+        }
+    }
+    console.log(amigos);
+}
+
+function sortearAmigo() {
+    let sorteio = Math.floor(Math.random() * amigos.length);
+    let nomeSorteado = document.createElement('li');
+    let mensagemAmigoSorteado = `O amigo(a) sorteado foi ${amigos[sorteio]}`;
+    nomeSorteado.innerHTML = mensagemAmigoSorteado;
+    amigoSorteado.append(nomeSorteado);
+    limparListaDeAmigos();
+    amigos = [];
+}
+
+function limparCampo() {
+    campoAmigos.value = '';
+}
+
+function limparListaDeAmigos() {
+    listaAmigos.remove();
+}
+
+function mostrarAmigoNaLista(amigo) {
+    item = document.createElement('li');
+    item.innerHTML = amigo;
+    listaAmigos.appendChild(item);
+    console.log(item);
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        btn = document.querySelector('.button-add');
+        btn.click();
+    }
+}) 
